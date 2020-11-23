e354d436add2e18db3a33cc0c787f79f77c86fdd
KeiM2120 <kei.mmpublic@gmail.com>
2020-11-24 08:44:45 +0900

    add details Event.vue

--- app/src/components/Event.vue
+++ app/src/components/Event.vue
@@ -1,7 +1,17 @@
 <template>
     <div id="Event">
+        param: {{ $route.params.id }}
+        <div>
+            <h2> {{ details.name }}</h2>
+            {{ details.date }}
 
+            <h5>担当講師: {{ details.teacher }} </h5>
+            <div>
+                {{ details.desc }}
+            </div>
+        </div>
     </div>
+    <!-- 参加ボタンどうしよう -->
 </template>
 
 <style lang="scss">
@@ -9,7 +19,19 @@
 </style>
 
 <script>
+// $route.params.id でイベントidを取得して該当する情報をバックから引き出す
 export default {
-    
+    name: 'event',
+    data: ()=> {
+        return{
+            details: {
+                id: "001",
+                name: "イベントA",
+                date: "2020/11/11",
+                teacher: "近大 弘一",
+                desc: "本イベントは〇〇をして△△を目指すものになります。"
+            }
+        }
+    }
 }
 </script>
