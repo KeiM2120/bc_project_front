edd48518e184faa83f247bd7f8b96b070b5d7ba8
KeiM2120 <kei.mmpublic@gmail.com>
2020-11-24 16:24:12 +0900

    add Profile.vue

--- app/src/components/Profile.vue
+++ app/src/components/Profile.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="profile">
-
+        <h2>プロフィール編集</h2> <button>完了</button>
+        <div id="form">
+            <input type="text" name="username" placeholder="ユーザ名"><br/>
+            <input type="text" name="mailadress" placeholder="メールアドレス"><br/>
+            <input type="password" name="password" placeholder="パスワード"><br/>
+            <input type="text" name="introduction" placeholder="自己紹介"><br/>
+        </div>
     </div>
 </template>
 
