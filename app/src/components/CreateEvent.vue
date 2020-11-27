9c9142f7513fadc474a3d7b6b47d4d18608adfb1
KeiM2120 <kei.mmpublic@gmail.com>
2020-11-27 15:27:22 +0900

    add createevent page

--- app/src/components/CreateEvent.vue
+++ app/src/components/CreateEvent.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="createevent">
+        <h2>イベント新規作成</h2>
+        <div id="eventform">
+            イベント名 <input type="text" name="eventname"><br/>
+            <!-- タグ入力・選択 -->
+            開催日 <input type="date" name="eventdate"><br/>
+            講師選択 :講師選択方法要検討:<br/>
+            イベント説明 <input type="text" name="eventdesc"><br/>
 
+        </div>
     </div>
 </template>
 
