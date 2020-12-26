c8a36c2acec8ead399de9b172d541d1b80894a4d
KeiM2120 <kei.mmpublic@gmail.com>
2020-12-26 12:55:31 +0900

    feature: add fetch mypage data for loading mypage

--- app/src/components/Mypage.vue
+++ app/src/components/Mypage.vue
@@ -3,13 +3,25 @@
         <h2>マイページ</h2>
         <div>
             <!-- ユーザのアイコン(実装できれば) -->
-            {{username}}<br/>
-            {{occupation}}
+            <div>ユーザ名 <b>{{ userData.username }}</b></div>
+            <div>職業 {{ userData.profession }}</div>
         </div>
+        
         <div>
+            <h4>評価</h4>
+            <div>
+                <h6>固定軸評価</h6>
+                <ul>
+                    <li>前に踏み出す力: {{ avgAct }}</li>
+                    <li>考え抜く力: {{ avgThink }}</li>
+                    <li>チームで働く力: {{ avgTeam }}</li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- <div>
             参加イベント
             <div id="eventlist">
-                <!-- v-forでの列挙 -->
                 <ul>
                     <li v-for="event in events" v-bind:key="event.id">
                         {{ event.id }}<br/>
@@ -18,7 +30,7 @@
                     </li>
                 </ul>
             </div>
-        </div>
+        </div> -->
         <a href="./">top</a>
     </div>
 </template>
@@ -28,7 +40,7 @@
 <script>
 export default{
     name: 'mypage',
-    data: ()=>{
+    data: function(){
         return{
             userData: {},
             username: "ユーザ名",
@@ -40,17 +52,25 @@ export default{
                 ]
         }
     },
-    created: async ()=>{
-        await fetch('/api/mypage')
-        .then(response=>response.json())
-        .then(data=>{
-            console.log(data)
+    created: function(){
+        let self= this;
+        fetch('/api/mypage')
+        .then(function(response){return response.json()})
+        .then(function(jData){
+            console.log(jData)
+            self.userData= jData
         })
-        .catch((e)=> {
-            console.log(e)
-            location.href='/'
-            }
-        )
+    },
+    computed: {
+        avgAct: function(){
+            return ( this.userData.ts[0].action+ this.userData.ss[0].action)/2
+        },
+        avgThink: function(){
+            return ( this.userData.ts[1].think + this.userData.ss[1].think)/2
+        },
+        avgTeam: function(){
+            return ( this.userData.ts[2].team+ this.userData.ss[2].team)/2
+        }
     }
 }
 </script>
