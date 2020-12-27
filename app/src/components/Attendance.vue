781100df3b17cddafb586a73e7f56652b76dcf62
KeiM2120 <kei.mmpublic@gmail.com>
2020-12-27 17:37:04 +0900

    feature: list attendances to select evaluatin user

--- app/src/components/Attendance.vue
+++ app/src/components/Attendance.vue
@@ -1,13 +1,14 @@
 <template>
     <div id="attendance">
         param: {{ $route.params.event_id }}
-        <h2> {{ eventname }} </h2>
+        <h2> {{ eventdata.event_name }} </h2>
         <div id="attendancelist">
             <h4>イベント参加者</h4>
             <div>
                 <ul>
                     <li v-for="user in attendance" v-bind:key="user.id">
-                        {{ user.name }} <button name="button_evaluate">評価</button>
+                        <!-- 参加者リスト出力. 利用しているユーザid(self_id)と一致するユーザには評価を送れないようにしている. -->
+                        {{ user.user_name }} <button v-if="(mode==0)&&(user.id!=self_id)" name="button_evaluate" @click="evaluateUser(user.id)">評価</button>
                     </li>
                 </ul>
             </div>
@@ -25,16 +26,30 @@ export default {
     name: 'attendance',
     data: ()=>{
         return{
-            eventname: 'イベントA',
+            eventdata:{},
+            mode: -1,
+            self_id: -1,
             attendance: [
-                {id: "001",name: "近大太郎"},
-                {id: "003",name: "近大次郎"},
-                {id: "005",name: "近大三郎"},
-                {id: "011",name: "john"},
-                {id: "014",name: "lis"},
+                // {id: "001",name: "近大太郎"},
+                // {id: "003",name: "近大次郎"},
+                // {id: "005",name: "近大三郎"},
+                // {id: "011",name: "john"},
+                // {id: "014",name: "lis"},
 
             ],
         }
-    }
+    },
+    methods: {
+        evaluateUser: function(user_id){
+            alert('called evalueteUser user_id:'+ user_id)
+        }
+    },
+    created: async function(){
+        let resData= await fetch('/api/participates?event_id='+ this.$route.params.event_id).then(response=>response.json())
+        this.mode= resData.branch
+        this.attendance= resData.students
+        this.eventdata= resData.eventlist[0]
+        this.self_id= resData.self_id
+    },
 }
 </script>
