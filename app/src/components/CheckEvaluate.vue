d97fb49ce890ec51adce7e6f070dc5ebca99e178
KeiM2120 <kei.mmpublic@gmail.com>
2021-01-13 16:52:21 +0900

    feature: add check page for any user

--- app/src/components/CheckEvaluate.vue
+++ app/src/components/CheckEvaluate.vue
@@ -7,26 +7,46 @@
         
         <h2>評価確認</h2>
         <div id="evaluation">
-            <div>
-            <h3>イベント名: {{ evaluation.eventlist[0].event_name }} </h3>
-            <h4>評価</h4>
-            <div>
-                <h6>固定軸評価</h6>
-                <ul>
-                    <li>前に踏み出す力: {{ avgAct }}</li>
-                    <li>考え抜く力: {{ avgThink }}</li>
-                    <li>チームで働く力: {{ avgTeam }}</li>
-                </ul>
+            <div v-if="mode == 'e'">
+                <h3>イベント名: {{ evaluation.eventlist[0].event_name }} </h3>
+                <h4>評価</h4>
+                <div>
+                    <h6>固定軸評価</h6>
+                    <ul>
+                        <li>前に踏み出す力: {{ avgAct }}</li>
+                        <li>考え抜く力: {{ avgThink }}</li>
+                        <li>チームで働く力: {{ avgTeam }}</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6>自由軸評価</h6>
+                    <ul>
+                        <li v-for="(v, k) in freeEval" v-bind:key="k">
+                            {{ k }} : {{ v }}
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h6>自由軸評価</h6>
-                <ul>
-                    <li v-for="(v, k) in freeEval" v-bind:key="k">
-                        {{ k }} : {{ v }}
-                    </li>
-                </ul>
+            <div v-else-if="mode == 'u'">
+                <h3>氏名: {{ evaluation.username }} </h3>
+                <h4>評価</h4>
+                <div>
+                    <h6>固定軸評価</h6>
+                    <ul>
+                        <li>前に踏み出す力: {{ avgAct }}</li>
+                        <li>考え抜く力: {{ avgThink }}</li>
+                        <li>チームで働く力: {{ avgTeam }}</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6>自由軸評価</h6>
+                    <ul>
+                        <li v-for="(v, k) in freeEval" v-bind:key="k">
+                            {{ k }} : {{ v }}
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 </template>
@@ -53,17 +73,26 @@ export default {
         else if ( m === 'e'){this.mode= m}
         else {location.href= '/mypage'}
 
-        let edata=  await fetch('/api/eConfirmation?event_id='+ this.$route.params.id).then(response=>response.json())
+        let edata;
+        if(m === 'e'){
+            edata=  await fetch('/api/eConfirmation?event_id='+ this.$route.params.id).then(response=>response.json())
+        }
+        else if(m === 'u'){
+            edata= await fetch('/api/saiyoumypage?email='+ this.$route.params.id).then(response=> response.json())
+        }
         this.evaluation= edata
     },
     computed: {
         avgAct: function(){
+            if(this.evaluation.ts === undefined){return -1}
             return ( this.evaluation.ts[0].action+ this.evaluation.ss[0].action)/2
         },
         avgThink: function(){
+            if(this.evaluation.ts === undefined){return -1}
             return ( this.evaluation.ts[1].think + this.evaluation.ss[1].think)/2
         },
         avgTeam: function(){
+            if(this.evaluation.ts === undefined){return -1}
             return ( this.evaluation.ts[2].team+ this.evaluation.ss[2].team)/2
         },
         // 自由軸評価が12種の前提
